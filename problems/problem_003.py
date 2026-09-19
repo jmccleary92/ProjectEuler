dbef1bfed10dfa7c1f763e9ae87576c82b126432
Josh McCleary <josh.mccleary39@gmail.com>
2026-09-19 16:12:15 -0400

    Adding gitignore and updating problems.

--- problems/problem_003.py
+++ problems/problem_003.py
@@ -6,21 +6,15 @@ The prime factors of 13195 are 5,7,13 and 29.
 What is the largest prime factor of the number 600851475143?
 """
 
-def solve() -> int:
-    n = 600851475143
-    factor = 2
-    last_factor = 1
+n = 600851475143
+factor = 3
+largest_factor = 1
 
-    while n > 1:
-        if n % factor == 0:
-            last_factor = factor
+while n > 1:
+    if n % factor == 0:
+        largest_factor = factor
+        while n % factor == 0:
             n //= factor
-            while n % factor == 0:
-                n //= factor
-        factor += 1 if factor == 2 else 2  # Increment by 1 if 2, else by 2 to check only odd numbers
+        factor += 2
 
-    return last_factor
-
-
-if __name__ == "__main__":
-    print(solve())
+print(largest_factor)
